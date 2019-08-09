6b087ed4baecf2f54d55f6e8f0fe14dd550179fd
James <40544507+Jacherr@users.noreply.github.com>
2019-08-09 17:06:07 +0100

    Adding the key elements of the API (#1)
    This PR is the general logic of the API.
    It acts as a connector between a user and rextester API (an API to evaluate code for different language in a sandboxed environment).
    It facilitates requesting to rextester by adding default imports and managing those imports for you.
    It also creates the default structure for the code (main function / main class etc)

---
+++ index.js
@@ -0,0 +1,42 @@
+const langProp = require('./src/languageProperties.json');
+const { handleRequest } = require('./src/rextester.js');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { port } = require('./config.json');
+
+
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded( {
+    extended: true,
+} ) );
+
+app.get('/languages', (req, res) => {
+    const langArray = [];
+    let currentLangAliases,
+        currentLangImports;
+    langProp.languageProperties.forEach( (i) => {
+        currentLangAliases = i.aliases.join(', ');
+        currentLangImports = i.defaultImports.join(', ');
+        langArray.push(`Language: ${i.commonName};\nAliases: ${currentLangAliases};\nImports: ${currentLangImports}`);
+    } );
+    return res.send(langArray.join('\n\n') ).end();
+} );
+
+app.post('/', (req, res) => {
+    // expecting body = {lang: 'language' < STR >, code: 'code' < STR >, imports: ['imports'] < ARR >}
+    // example req = superagent.post('api').send({lang: 'java', code: 'System.out.println("hello world")', imports: ['System']});
+    handleRequest(req.body.lang, req.body.code, req.body.imports, (request) => {
+        // eslint-disable-next-line no-magic-numbers
+        if (request.code !== 200) {
+            res.status(request.code);
+            return res.send(`${request.payload}`);
+        }
+        if (request.error === true) {
+            return res.send(request.payload);
+        }
+        return res.send(request.payload);
+    } );
+} );
+
+app.listen(port, () => console.log(`running on port ${port}`) );
---
+++ src/rextester.js
@@ -0,0 +1,119 @@
+const languageProperties = require('./languageProperties.json');
+const superagent = require('superagent');
+
+function parseCode(code, languageObject, imports) {
+    if (languageObject.classDeclaration) { // Add class declaration and replace {{code}} with the user's code
+        let classDec = languageObject.classDeclaration;
+        classDec = classDec.replace('{{code}}', code.join(' ') );
+        code = classDec;
+    } else {
+        code = code.join(' ');
+    }
+    imports.forEach( (element, i) => { // If import already exists in defaults then remove it from the array
+        if (languageObject.defaultImports.includes(element) ) {
+            imports.splice(i, 1);
+        }
+    } );
+    languageObject.defaultImports.forEach(element => { // Add the default imports the the imports array
+        imports.push(element);
+    } );
+    imports.forEach(element => { // Add the imports to the top of the code
+        code = `${languageObject.importType} ${element}${languageObject.lineBreak}\n${code}`;
+    } );
+    return code;
+}
+
+async function outputResult(language, code, compilerArgs, callback) {
+    let requestResponse,
+        rextesterResponse;
+    try {
+        rextesterResponse = await superagent
+            .post('https://rextester.com/rundotnet/api')
+            .set( {
+                'Content-Type': 'application/json',
+            } )
+            .send( {
+                LanguageChoice: language,
+                Program: code,
+                Input: '',
+                CompilerArgs: compilerArgs,
+            } );
+    } catch (err) {
+        requestResponse = {
+            payload: err.toString(),
+            error: false,
+            code: 400,
+        };
+    }
+    if (rextesterResponse.body.Result) {
+        if (rextesterResponse.body.Result.length === 0) {
+            requestResponse = {
+                payload: 'Empty response',
+                error: false,
+                code: 200,
+            };
+        } else {
+            requestResponse = {
+                payload: rextesterResponse.body.Result,
+                error: false,
+                code: 200,
+            };
+        }
+    } else if (rextesterResponse.body.Errors) {
+        if (rextesterResponse.body.Errors.length === 0) {
+            requestResponse = {
+                payload: 'Empty response (errored)',
+                error: true,
+                code: 200,
+            };
+        } else {
+            requestResponse = {
+                payload: rextesterResponse.body.Errors,
+                error: true,
+                code: 200,
+            };
+        }
+    }
+    return callback(requestResponse);
+}
+
+async function handleRequest(language, code, imports, callback) {
+    let compilerArgs;
+
+    if (!code || !code[0] ) {
+        return callback( {
+            payload: 'no code/lang???',
+            error: false,
+            code: 400,
+        } );
+    }
+
+    code = code.split(' ');
+
+    const languageObject = languageProperties.languageProperties.find(curobject => curobject.aliases.includes(language.toString() ) );
+
+    if (!languageObject) return callback( {
+        payload: 'Unsupported language',
+        error: false,
+        code: 400,
+    } );
+
+    code.join(' ');
+    if (languageObject.defaultImports.length > 0 || languageObject.classDeclaration || imports.length > 0) { // If there is a class to declare or if there are default imports then add them to code
+        code = await parseCode(code, languageObject, imports);
+    }
+
+    if (languageObject.languageCode) language = languageObject.languageCode;
+    if (languageObject.compilerArgs) {
+        compilerArgs = languageObject.compilerArgs.join(' ');
+    } else {
+        compilerArgs = '';
+    }
+
+    outputResult(language, code, compilerArgs, (output) => callback(output) );
+    return 0;
+}
+
+module.exports = {
+    handleRequest,
+};
