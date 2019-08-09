a1a053d3c13d9a0d92f3874d884ee500bc1f563b
Jacher <jwc2002@outlook.com>
2019-08-09 16:18:40 +0100

    Final changes made

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const langProp = require('./src/languageProperties.json');
-const { handleRequest } = require('./src/rextester.js')
+const { handleRequest } = require('./src/rextester.js');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = require('./config.json').port
+const { port } = require('./config.json');
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded( {
+    extended: true,
+} ) );
 
 app.get('/languages', (req, res) => {
-    let langArray = [];
-    let currentLangAliases;
-    let currentLangImports;
-    langProp.languageProperties.forEach(function(i){
-        currentLangAliases = i.aliases.join(", ")
-        currentLangImports = i.defaultImports.join(", ")
-        langArray.push(`Language: ${i.commonName};\nAliases: ${currentLangAliases};\nImports: ${currentLangImports}`)
-    });
-    return res.send(langArray.join('\n\n')).end();
-})
+    const langArray = [];
+    let currentLangAliases,
+        currentLangImports;
+    langProp.languageProperties.forEach( (i) => {
+        currentLangAliases = i.aliases.join(', ');
+        currentLangImports = i.defaultImports.join(', ');
+        langArray.push(`Language: ${i.commonName};\nAliases: ${currentLangAliases};\nImports: ${currentLangImports}`);
+    } );
+    return res.send(langArray.join('\n\n') ).end();
+} );
 
-app.post('/', async function (req, res) {
+app.post('/', async(req, res) => {
     // expecting body = {lang: 'language' < STR >, code: 'code' < STR >}
-    // example req = superagent.post('api').send({lang: 'java', code: 'System.out.println("hello world")'});
-    await handleRequest(req.body.lang, req.body.code, req.body.imports, function(request) {
-        if (request.code != 200) {
-            res.status(request.code)
+    // example req = superagent.post('api').send({lang: 'java', code: 'System.out.println("hello world")', imports: ['System']});
+    await handleRequest(req.body.lang, req.body.code, req.body.imports, (request) => {
+        // eslint-disable-next-line no-magic-numbers
+        if (request.code !== 200) {
+            res.status(request.code);
             return res.send(`${request.payload}`);
         }
-        else if (request.error == true) {
+        if (request.error === true) {
             return res.send(request.payload);
-        } else {
-            return res.send(request.payload);
-        };
-    });
-});
+        }
+        return res.send(request.payload);
+    } );
+} );
 
-app.listen(port, () => console.log(`running on port ${port}`))
+app.listen(port, () => console.log(`running on port ${port}`) );
--- src/rextester.js
+++ src/rextester.js
@@ -1,114 +1,112 @@
 const languageProperties = require('./languageProperties.json');
-const superagent = require('superagent')
+const superagent = require('superagent');
 
 function parseCode(code, languageObject, imports) {
-    if (languageObject.classDeclaration) { //Add class declaration and replace {{code}} with the user's code
-        let classDec = languageObject.classDeclaration
-        classDec = classDec.replace("{{code}}", code.join(" "))
-        code = classDec
-    } else code = code.join(" ")
-    let i = 0
-    imports.forEach(element => { //If import already exists in defaults then remove it from the array
-        if (languageObject.defaultImports.includes(element)) {
-            imports.splice(i, 1)
+    if (languageObject.classDeclaration) { // Add class declaration and replace {{code}} with the user's code
+        let classDec = languageObject.classDeclaration;
+        classDec = classDec.replace('{{code}}', code.join(' ') );
+        code = classDec;
+    } else code = code.join(' ');
+    imports.forEach( (element, i) => { // If import already exists in defaults then remove it from the array
+        if (languageObject.defaultImports.includes(element) ) {
+            imports.splice(i, 1);
         }
-        i++
-    });
-    languageObject.defaultImports.forEach(element => {  //Add the default imports the the imports array
-        imports.push(element)
-    })
-    imports.forEach(element => { //Add the imports to the top of the code
-        code = `${languageObject.importType} ${element}${languageObject.lineBreak}\n${code}`
-    });
-    return code
+    } );
+    languageObject.defaultImports.forEach(element => { // Add the default imports the the imports array
+        imports.push(element);
+    } );
+    imports.forEach(element => { // Add the imports to the top of the code
+        code = `${languageObject.importType} ${element}${languageObject.lineBreak}\n${code}`;
+    } );
+    return code;
 }
 
 async function outputResult(language, code, compilerArgs, callback) {
-    let r;
-    let response = await superagent
-        .post('https://rextester.com/rundotnet/api')
-        .set({
-            "Content-Type": "application/json"
-        })
-        .send({
-            LanguageChoice: language,
-            Program: code,
-            Input: "",
-            CompilerArgs: compilerArgs
-        })
-        .catch((err) => r = {
+    let r,
+        response;
+    try {
+        response = await superagent
+            .post('https://rextester.com/rundotnet/api')
+            .set( {
+                'Content-Type': 'application/json',
+            } )
+            .send( {
+                LanguageChoice: language,
+                Program: code,
+                Input: '',
+                CompilerArgs: compilerArgs,
+            } );
+    } catch (err) {
+        r = {
             payload: err.toString(),
             error: false,
-            code: 400
-        });
+            code: 400,
+        };
+    }
     if (response.body.Result) {
-        if (response.body.Result.length == 0)
+        if (response.body.Result.length === 0)
             r = {
                 payload: 'Empty response',
                 error: false,
-                code: 200
-            }
+                code: 200,
+            };
         else
             r = {
                 payload: response.body.Result,
                 error: false,
-                code: 200
+                code: 200,
             };
-
     } else if (response.body.Errors) {
-        if (response.body.Errors.length == 0) r = {
-            payload: "Empty response (errored)",
+        if (response.body.Errors.length === 0) r = {
+            payload: 'Empty response (errored)',
             error: true,
-            code: 200
-        }
+            code: 200,
+        };
         else
             r = {
                 payload: response.body.Errors,
                 error: true,
-                code: 200
+                code: 200,
             };
-
     }
     return callback(r);
 }
 
 async function handleRequest(language, code, imports, callback) {
-    let languageObject;
     let compilerArgs;
 
-    if (!code || !code[0]) return callback({
+    if (!code || !code[0] ) return callback( {
         payload: 'no code/lang???',
         error: false,
-        code: 400
-    });
+        code: 400,
+    } );
 
     code = code.split(' ');
 
-    const languageObject = languageProperties.languageProperties.find(curobject => curobject.aliases.includes(language.toString()));
+    const languageObject = languageProperties.languageProperties.find(curobject => curobject.aliases.includes(language.toString() ) );
 
-    if (!languageObject) return callback({
+    if (!languageObject) return callback( {
         payload: 'Unsupported language',
         error: false,
-        code: 400
-    });
+        code: 400,
+    } );
 
-    code.join(" ")
-    if (languageObject.defaultImports.length > 0 || languageObject.classDeclaration || imports.length > 0) { //If there is a class to declare or if there are default imports then add them to code
-        code = await parseCode(code, languageObject, imports)
+    code.join(' ');
+    if (languageObject.defaultImports.length > 0 || languageObject.classDeclaration || imports.length > 0) { // If there is a class to declare or if there are default imports then add them to code
+        code = await parseCode(code, languageObject, imports);
     }
 
-    if(languageObject.languageCode) language = languageObject.languageCode
-    if(languageObject.compilerArgs) {
-        compilerArgs = languageObject.compilerArgs.join(" ")
+    if (languageObject.languageCode) language = languageObject.languageCode;
+    if (languageObject.compilerArgs) {
+        compilerArgs = languageObject.compilerArgs.join(' ');
     } else {
-        compilerArgs = ""
+        compilerArgs = '';
     }
 
-    outputResult(language, code, compilerArgs, async function (output) {
-        return await callback(output);
-    });
-};
+    outputResult(language, code, compilerArgs, async(output) => callback(output) );
+    return 0;
+}
 
 module.exports = {
-    handleRequest: handleRequest
-}
+    handleRequest,
+};
